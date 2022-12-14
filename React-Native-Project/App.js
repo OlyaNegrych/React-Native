6fbb1efa5d4cda0d4c6c1bab9d79d23ff51b4202
OlyaNegrych <olyanegrych@gmail.com>
2022-12-15 00:39:06 +0200

    added functionality to register form

--- React-Native-Project/App.js
+++ React-Native-Project/App.js
@@ -1,29 +1,22 @@
 // import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground, TextInput, Button } from "react-native";
-// import RegistrationScreen from './Screens/RegistrationScreen';
+import { StyleSheet, View, Keyboard, ImageBackground, TouchableWithoutFeedback } from "react-native";
+import RegistrationScreen from "./Screens/RegistrationScreen";
+// import LoginScreen from "./Screens/LoginScreen";
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        style={styles.image}
-        // source={require("../assets/images/bg_photo.jpg")}
-      >
-        <View style={styles.form}>
-          <View style={styles.userPhoto}></View>
-          <Text>Реєстрація</Text>
-          <TextInput style={styles.input} />
-          <TextInput style={{ ...styles.input, marginTop: 16 }} />
-          <TextInput
-            style={{ ...styles.input, marginTop: 16 }}
-            secureTextEntry={true}
-          />
-          <Button title="Показати" style={styles.show} />
-          <Button title="Зареєструватися" style={styles.register} />
-          <Button title="Вже є аккаунт? Увійти" style={styles.login} />
-        </View>
-      </ImageBackground>
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.image}
+          source={require("../assets/images/bg_photo.jpg")}
+        >
+          <RegistrationScreen />
+          {/* <LoginScreen /> */}
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -39,40 +32,5 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
     justifyContent: "center",
     alignItems: "center",
-  },
-  form: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    marginHorizontal: 40,
-    // borderWidth: 1,
-    // borderColor: "black",
-    borderRadius: 5,
-    // borderRadius: 25 25 0 0,
-  },
-  userPhoto: {},
-  input: {
-    width: 250,
-    paddingLeft: 16,
-    borderWidth: 1,
-    borderColor: "black",
-    height: 40,
-    borderRadius: 5,
-    backgroundColor: "#F6F6F6",
-    color: "black",
-  },
-  show: {
-    fontSize: 24,
-    backgroundColor: "white",
-  },
-  register: {
-    backgroundColor: "#FF6C00",
-    width: 250,
-    color: "white",
-    borderRadius: 100,
-  },
-  login: {
-    fontSize: 24,
   },
 });
