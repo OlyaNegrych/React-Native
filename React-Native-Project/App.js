d0c3e4a512be5acb1aceb3c0fc4d109cc276e855
OlyaNegrych <olyanegrych@gmail.com>
2022-12-14 12:28:58 +0200

    added initial styles

--- React-Native-Project/App.js
+++ React-Native-Project/App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TextInput } from "react-native";
+import RegistrationScreen from './Screens/RegistrationScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js !</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      style={styles.image}
+      // source={require("../assets/images/bg_photo.jpg")}
+    >
+      <View style={styles.container}>
+        <TextInput style={styles.input} />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'black',
+    marginHorizontal: 20,
+    height: 40,
+    borderRadius: 5
+  }
 });
